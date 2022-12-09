cda92288cc9bb6d236802748431b104521161d4d
Sondre Andersen <sonand@arkivverket.no>
2022-12-09 09:12:00 +0100

    Update

--- app1/app1/main.py
+++ app1/app1/main.py
@@ -1,8 +1,8 @@
-from lib1 import archive_something, function_only_in_version_gt_0_0_1
+from lib1 import archive_something, function_only_in_version_gt_0_0_4
 
 
 if __name__ == "__main__":
     print("Starter main")
     print("Bruker funksjon fra lib1 pakke via Github.")
     archive_something()
-    function_only_in_version_gt_0_0_1
+    function_only_in_version_gt_0_0_4()
